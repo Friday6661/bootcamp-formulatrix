d2bc309f0c0ff2dccd34681b52747a099d0d0605
Friday6661 <jarifr26@gmail.com>
2023-05-10 12:18:39 +0700

    Penambahan program ClassObject, dan Constructor

---
+++ latihanhari001/ClassObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace DemoClassObject {
+    class Program {
+        static void Main(string[] args) {
+                    
+                    // Membuat Object
+                    Calculator calObject = new Calculator();
+
+                    // Akses Calculator class member menggunakan Calculator class object
+                    int result = calObject.CalculateSum(10,20);
+                    Console.WriteLine(result);
+                    Console.ReadKey();
+                }
+    }
+
+    // Define Class atau blueprint atau template
+    public class Calculator {
+        public int CalculateSum(int a, int b) {
+            return a + b;
+        }
+    }
+}
