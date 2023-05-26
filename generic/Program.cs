410f37b4e68dc92de2ab12de7c07ddc00fbea695
Friday6661 <jarifr26@gmail.com>
2023-05-26 12:18:55 +0700

    latihan generic

---
+++ generic/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using ItemLib;
+using InventoryLib;
+
+class Program
+{
+    static void Main()
+    {
+        Inventory<Item> inventory = new Inventory<Item>();
+
+        Item sword = new Item("Sword");
+        Item potion = new Item("Potion");
+        Item shield = new Item("Shield");
+
+        Console.WriteLine(inventory.AddItem(sword));
+        Console.WriteLine(inventory.AddItem(potion));
+        Console.WriteLine(inventory.AddItem(shield));
+        Console.WriteLine(inventory.DisplayItems());
+
+        Console.WriteLine(inventory.RemoveItem(potion));
+    }
+}
