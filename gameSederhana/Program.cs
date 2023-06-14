7a90ffdb0559bec70880da47e9dca7244b07eb3c
Friday6661 <jarifr26@gmail.com>
2023-06-14 13:06:08 +0700

    commit laptop bootcamp

--- gameSederhana/Program.cs
+++ gameSederhana/Program.cs
@@ -1,5 +1,6 @@
 ﻿using gameLib;
-
+using System.Diagnostics;
+using System.IO;
 
 class Program {
     static void Main(string[] args) {
@@ -15,5 +16,21 @@ class Program {
         game.EndGame();
 
         Console.ReadLine();
+        // Clear any existing trace listeners
+			Trace.Listeners.Clear();
+			
+			// Create a new TextWriterTraceListener to write output to a file
+			TextWriterTraceListener traceListener = new TextWriterTraceListener("myTraceOutput.txt");
+			
+			// Add the trace listener to the Trace object
+			Trace.Listeners.Add(traceListener);
+			Trace.Assert(false, "This is a trace false.");
+			Debug.Assert(false, "This is a DEBUG FALSE.");
+			// Write a trace statement to output something to the file
+			Trace.WriteLine("This is a trace statement.");
+			
+			// Flush and close the trace listener to ensure all output is written to the file
+			traceListener.Flush();
+			traceListener.Close();
     }
 }
